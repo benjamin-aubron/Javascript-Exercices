5017dbccb9ad0bce4e8eac6dcc34655e153c148f
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-01-17 16:48:40 +0100

    js finished

--- combinaison.js
+++ combinaison.js
@@ -12,11 +12,11 @@ function factorial(n) {
   if (n<0){
     return false
   }
-  else if (n == 0){
+  else if (n == 0 || n == 1){
     return 1
   }
   else {
-    return (n != 1) ? n * factorial(n - 1) : 1; // A réécrire
+    return n * factorial(n- 1)
   }
 }
 
