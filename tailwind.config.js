a8931a311a06e33d60aaff254b459e582bf4197d
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-02-25 17:01:49 +0100

    git reset --hard HEAD^

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
