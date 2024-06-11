33de732b7e728b2a39609b4e3aed7cb2f8ac8c8f
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-06-11 19:13:56 +0200

    projet_12 - done

--- src/projet_12/projet_12.js
+++ src/projet_12/projet_12.js
@@ -21,8 +21,13 @@ const characterSet = {
     addASet(33, 47) + addASet(58, 64) + addASet(91, 96) + addASet(123, 126),
 };
 
+const range = document.querySelector("input[type='range']");
+const rangeLabel = document.querySelector(".range-group label");
+rangeLabel.textContent = `Taille du mot de passe : ${range.value}`;
+let passwordLength = range.value;
+
 const passwordContent = document.querySelector(".password-content");
-const errorMsg = document.querySelector(".error.msg");
+const errorMsg = document.querySelector(".error-msg");
 const generateButton = document.querySelector(".generate-password-btn");
 const checkboxes = document.querySelectorAll("input[type='checkbox']");
 
@@ -30,6 +35,36 @@ generateButton.addEventListener("click", createPassword);
 
 function createPassword() {
   const checkedDataSets = checkedSets();
+
+  if (!checkedDataSets.length) {
+    errorMsg.textContent = "Au moins une case doit être cochée !";
+    return;
+  } else errorMsg.textContent = "";
+
+  const concatenatedDataSets = checkedDataSets.reduce((acc, cur) => acc + cur);
+
+  let password = "";
+  let passwordBase = [];
+  for (let i = 0; i < checkedDataSets.length; i++) {
+    passwordBase.push(
+      checkedDataSets[i][getRandomNumber(0, checkedDataSets[i].length - 1)]
+    );
+  }
+
+  for (let i = checkedDataSets.length; i < passwordLength; i++) {
+    password +=
+      concatenatedDataSets[getRandomNumber(0, concatenatedDataSets.length - 1)];
+  }
+
+  passwordBase.forEach((item, index) => {
+    const randomIndex = getRandomNumber(0, password.length);
+    password =
+      password.slice(0, randomIndex) +
+      passwordBase[index] +
+      password.slice(randomIndex);
+  });
+
+  passwordContent.textContent = password;
 }
 
 function checkedSets() {
@@ -38,5 +73,35 @@ function checkedSets() {
     (checkbox) =>
       checkbox.checked && checkedSets.push(characterSet[checkbox.id])
   );
-  return checkedSets
+  return checkedSets;
 }
+
+createPassword();
+
+
+range.addEventListener("input",handleRange)
+
+function handleRange(e){
+  passwordLength = e.target.value;
+  rangeLabel.textContent = `Taille du mot de passe : ${e.target.value}`;
+}
+
+const copyBtn = document.querySelector(".copy-btn")
+copyBtn.addEventListener("click", copyPassword)
+
+let blocked = false
+function copyPassword(){
+  
+  if (!blocked) {
+    navigator.clipboard.writeText(passwordContent.textContent)
+    copyBtn.classList.add("active")
+    blocked = true
+
+    setTimeout(()=>{
+      blocked = false
+      copyBtn.classList.remove("active")
+    },600)
+  }
+
+}
+
